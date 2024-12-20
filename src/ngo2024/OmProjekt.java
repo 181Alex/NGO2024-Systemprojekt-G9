2392c815479555e5984111dd2c14593cd56a9e69
FridaSel <frida.selin@gmail.com>
2024-12-20 22:10:03 +0100

    Update for the OmProjekt class

--- src/ngo2024/OmProjekt.java
+++ src/ngo2024/OmProjekt.java
@@ -23,10 +23,19 @@ public class OmProjekt extends javax.swing.JFrame {
         initComponents();
         this.projektId = projektId;
         this.idb = idb;
-        lblH1ProjNamn.setText(ProjNamn(projektId));
+        
+        lblH1ProjNamn.setText(getProjNamn(projektId));
+        lblBeskrivning.setText(getProjBeskrivning(projektId));
+        lblPrioritet.setText(getProjPrio(projektId));
+        lblStatus.setText(getProjStatus(projektId));
+        lblSlutDatum.setText(getStartdatum(projektId));
+        lblStartDatum.setText(getSlutdatum(projektId));
+        lblKostnad.setText(getKostnad(projektId));
+        lblLand.setText(getLand(projektId));
+        lblProjektChef.setText(getProjektChef(projektId));
     }
     
-    private String ProjNamn(String projektId){
+    private String getProjNamn(String projektId){
         String projektNamn=" ";
         try{
             String sqlFraga = "SELECT projektnamn FROM projekt where pid = '" + projektId + "'";
@@ -36,6 +45,104 @@ public class OmProjekt extends javax.swing.JFrame {
         }
 
         return projektNamn;
+    }
+    
+    private String getProjBeskrivning(String projektId){
+        String projektBeskrivning=" ";
+        try{
+            String sqlFraga = "SELECT beskrivning FROM projekt where pid = '" + projektId + "'";
+        projektBeskrivning = idb.fetchSingle(sqlFraga);
+        } catch (InfException ex){
+           System.out.println(ex.getMessage());
+        }
+
+        return projektBeskrivning;
+    }
+    
+    private String getProjPrio(String projektId){
+        String projektPrio=" ";
+        try{
+            String sqlFraga = "SELECT prioritet FROM projekt where pid = '" + projektId + "'";
+        projektPrio = idb.fetchSingle(sqlFraga);
+        } catch (InfException ex){
+           System.out.println(ex.getMessage());
+        }
+
+        return projektPrio;
+    }
+    
+    private String getProjStatus(String projektId){
+        String projektStatus=" ";
+        try{
+            String sqlFraga = "SELECT status FROM projekt where pid = '" + projektId + "'";
+        projektStatus = idb.fetchSingle(sqlFraga);
+        } catch (InfException ex){
+           System.out.println(ex.getMessage());
+        }
+
+        return projektStatus;
+    }
+    
+    private String getStartdatum(String projektId){
+        String projektStart=" ";
+        try{
+            String sqlFraga = "SELECT startdatum FROM projekt where pid = '" + projektId + "'";
+        projektStart = idb.fetchSingle(sqlFraga);
+        } catch (InfException ex){
+           System.out.println(ex.getMessage());
+        }
+
+        return projektStart;
+    }
+    
+    private String getSlutdatum(String projektId){
+        String projektSlut=" ";
+        try{
+            String sqlFraga = "SELECT slutdatum FROM projekt where pid = '" + projektId + "'";
+        projektSlut = idb.fetchSingle(sqlFraga);
+        } catch (InfException ex){
+           System.out.println(ex.getMessage());
+        }
+
+        return projektSlut;
+    }
+    
+    private String getKostnad(String projektId){
+        String projektKostnad=" ";
+        try{
+            String sqlFraga = "SELECT kostnad FROM projekt where pid = '" + projektId + "'";
+        projektKostnad = idb.fetchSingle(sqlFraga);
+        } catch (InfException ex){
+           System.out.println(ex.getMessage());
+        }
+
+        return projektKostnad;
+    }
+    
+    private String getLand(String projektId){
+        String projektLand=" ";
+        try{
+            String sqlFraga = "SELECT namn FROM land JOIN projekt ON projekt.land = land.lid WHERE pid = '" + projektId + "'";
+        projektLand = idb.fetchSingle(sqlFraga);
+        } catch (InfException ex){
+           System.out.println(ex.getMessage());
+        }
+
+        return projektLand;
+    }
+    
+    
+    private String getProjektChef(String projektId){
+        String projektProjektChef=" ";
+        try{
+            String sqlFragaFornamn = "SELECT fornamn FROM anstalld JOIN projekt ON projekt.projektchef = anstalld.aid WHERE pid = '" + projektId + "'";
+            String sqlFragaEfternamn = "SELECT efternamn FROM anstalld JOIN projekt ON projekt.projektchef = anstalld.aid WHERE pid = '" + projektId + "'";
+            projektProjektChef = idb.fetchSingle(sqlFragaFornamn) + " " + idb.fetchSingle(sqlFragaEfternamn);
+        } catch (InfException ex){
+           System.out.println(ex.getMessage());
+        }
+
+        return projektProjektChef;
     }
 
     
@@ -55,14 +162,43 @@ public class OmProjekt extends javax.swing.JFrame {
         lblH2Status = new javax.swing.JLabel();
         lblPrioritet = new javax.swing.JLabel();
         lblStatus = new javax.swing.JLabel();
+        lblH2Kostnad = new javax.swing.JLabel();
+        lblH2Partners = new javax.swing.JLabel();
+        lblSlutDatum = new javax.swing.JLabel();
+        lblStartDatum = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        lblLand = new javax.swing.JLabel();
+        lblKostnad = new javax.swing.JLabel();
+        lblH2ProjChef = new javax.swing.JLabel();
+        lblProjektChef = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
+        lblH1ProjNamn.setFont(new java.awt.Font("Typo Round Bold Demo", 1, 18)); // NOI18N
+        lblH1ProjNamn.setForeground(new java.awt.Color(102, 0, 102));
+
+        lblBeskrivning.setFont(new java.awt.Font("Segoe UI", 2, 12)); // NOI18N
         lblBeskrivning.setText("Beskrivning");
 
         lblH2Prio.setText("Prioritet:");
 
         lblH2Status.setText("Status:");
+
+        lblH2Kostnad.setText("Kostnad:");
+
+        lblH2Partners.setText("Samarbetspartners:");
+
+        lblSlutDatum.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
+
+        lblStartDatum.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("-");
+
+        jLabel2.setText("Land:");
+
+        lblH2ProjChef.setText("Projektledare:");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -70,34 +206,84 @@ public class OmProjekt extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(31, 31, 31)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(lblBeskrivning, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(lblH1ProjNamn, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(lblH2Status, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(lblH2ProjChef, javax.swing.GroupLayout.PREFERRED_SIZE, 82, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(lblStatus, javax.swing.GroupLayout.PREFERRED_SIZE, 136, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(30, 30, 30)
-                        .addComponent(lblH2Prio, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(lblPrioritet, javax.swing.GroupLayout.PREFERRED_SIZE, 136, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(336, Short.MAX_VALUE))
+                        .addComponent(lblProjektChef, javax.swing.GroupLayout.PREFERRED_SIZE, 211, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(lblBeskrivning, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(lblH1ProjNamn, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addComponent(lblStartDatum, javax.swing.GroupLayout.PREFERRED_SIZE, 66, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(lblSlutDatum, javax.swing.GroupLayout.PREFERRED_SIZE, 66, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                            .addGroup(layout.createSequentialGroup()
+                                                .addComponent(lblH2Status, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                                .addComponent(lblStatus, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                            .addComponent(lblH2Partners, javax.swing.GroupLayout.PREFERRED_SIZE, 112, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                            .addGroup(layout.createSequentialGroup()
+                                                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                                .addComponent(lblLand, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                        .addGap(37, 37, 37)
+                                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                            .addGroup(layout.createSequentialGroup()
+                                                .addComponent(lblH2Kostnad, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                                .addComponent(lblKostnad, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                            .addGroup(layout.createSequentialGroup()
+                                                .addComponent(lblH2Prio, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                                .addComponent(lblPrioritet, javax.swing.GroupLayout.PREFERRED_SIZE, 113, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                                .addGap(0, 23, Short.MAX_VALUE)))
+                        .addGap(337, 337, 337))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(37, 37, 37)
                 .addComponent(lblH1ProjNamn, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(29, 29, 29)
-                .addComponent(lblBeskrivning, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                        .addComponent(lblStartDatum, javax.swing.GroupLayout.DEFAULT_SIZE, 20, Short.MAX_VALUE)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(lblSlutDatum, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                        .addComponent(lblH2Status)
-                        .addComponent(lblH2Prio)
-                        .addComponent(lblPrioritet, javax.swing.GroupLayout.PREFERRED_SIZE, 16, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addComponent(lblStatus, javax.swing.GroupLayout.PREFERRED_SIZE, 16, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(429, Short.MAX_VALUE))
+                    .addComponent(lblH2ProjChef)
+                    .addComponent(lblProjektChef, javax.swing.GroupLayout.PREFERRED_SIZE, 16, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(lblBeskrivning, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                .addComponent(lblH2Status)
+                                .addComponent(lblH2Prio)
+                                .addComponent(lblPrioritet, javax.swing.GroupLayout.PREFERRED_SIZE, 16, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(lblStatus, javax.swing.GroupLayout.PREFERRED_SIZE, 16, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(176, 176, 176)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                .addComponent(lblH2Kostnad)
+                                .addComponent(jLabel2))
+                            .addComponent(lblLand, javax.swing.GroupLayout.PREFERRED_SIZE, 16, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addComponent(lblKostnad, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 16, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addComponent(lblH2Partners)
+                .addContainerGap(203, Short.MAX_VALUE))
         );
 
         pack();
@@ -139,11 +325,21 @@ public class OmProjekt extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel lblBeskrivning;
     private javax.swing.JLabel lblH1ProjNamn;
+    private javax.swing.JLabel lblH2Kostnad;
+    private javax.swing.JLabel lblH2Partners;
     private javax.swing.JLabel lblH2Prio;
+    private javax.swing.JLabel lblH2ProjChef;
     private javax.swing.JLabel lblH2Status;
+    private javax.swing.JLabel lblKostnad;
+    private javax.swing.JLabel lblLand;
     private javax.swing.JLabel lblPrioritet;
+    private javax.swing.JLabel lblProjektChef;
+    private javax.swing.JLabel lblSlutDatum;
+    private javax.swing.JLabel lblStartDatum;
     private javax.swing.JLabel lblStatus;
     // End of variables declaration//GEN-END:variables
 }
